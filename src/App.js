eb6f833c0249378c2087fd7018014b3d95db9a6b
Rohit Sharma <iamrs2207@gmail.com>
2023-05-22 13:48:40 -0400

    added api calls, custom hooks, and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Home from './Home';
 import Navbar from './Navbar';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/create" element={<Create/>} />
+            <Route path="/" element={<Home/>} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,15 @@
 import { useEffect, useState } from "react";
 import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    const handleDelete = (id) => {
-        setBlogs(blogs.filter((blog) => blog.id !== id))
-    }
-
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs").then((res) => {
-            if(!res.ok) {
-                throw Error('Could not fetch data');
-            }
-            return res.json();
-        }).then((data) => {
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        }).catch(err => {
-            setError(err.message);
-            setIsPending(false);
-        })
-    }, []);
-
-
-    // runs code on every render
-    // useEffect(() => {
-    //     console.log("use effect ran")
-
-    //     // return () => {
-    //     //     console.log("clean up function")
-    //     // }
-    // }, [blogs]); // dependency array is an optional arguement and decided the running of useEffect
+   const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
 
     return ( 
         <div className="home">
             { error && <div>{error}</div>}
             { isPending && <div>Loading...</div>}
-            { blogs && <Bloglist blogs={blogs} handleDelete={handleDelete} /> }
+            { blogs && <Bloglist blogs={blogs} /> }
         </div>
      );
 }
