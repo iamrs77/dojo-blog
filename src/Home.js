cd20612178d20be022a9d93c5ac009e74ceddd5a
Rohit Sharma <iamrs2207@gmail.com>
2023-05-22 10:54:05 -0400

    added get request to fetch the blogs data

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,34 @@ import { useEffect, useState } from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         setBlogs(blogs.filter((blog) => blog.id !== id))
     }
 
-    // runs code on every render
     useEffect(() => {
-        console.log("use effect ran")
+        fetch("http://localhost:8000/blogs").then((res) => {
+            return res.json();
+        }).then((data) => {
+            console.log(data);
+            setBlogs(data);
+        })
+    }, []);
 
-        // return () => {
-        //     console.log("clean up function")
-        // }
-    }, [blogs]); // dependency array is an optional arguement and decided the running of useEffect
+
+    // runs code on every render
+    // useEffect(() => {
+    //     console.log("use effect ran")
+
+    //     // return () => {
+    //     //     console.log("clean up function")
+    //     // }
+    // }, [blogs]); // dependency array is an optional arguement and decided the running of useEffect
 
     return ( 
         <div className="home">
-            <Bloglist blogs={blogs} handleDelete={handleDelete} />
+            { blogs && <Bloglist blogs={blogs} handleDelete={handleDelete} /> }
         </div>
      );
 }
